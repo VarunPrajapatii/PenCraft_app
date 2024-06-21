633ed99f32651b997c2696cfdc35a8833ccb43d2
Varun Prajapati <varunprajapati123@gmail.com>
2024-06-22 02:13:59 +0530

    added signup and signin routes

---
+++ README.md
@@ -0,0 +1,24 @@
+## THE STACK
+
+I'll be building a full stack blog website in the following technologies: 
+### Frontend
+- React: A JavaScript library for building user interfaces with a component-based architecture.
+### Backend
+- Cloudflare Workers: Serverless execution environment ensuring high performance and low latency.
+### Validation
+- Zod: TypeScript-first schema declaration and validation library.
+### Language
+- TypeScript: A strongly typed programming language that enhances JavaScript with static type checking.
+### ORM
+- Prisma: Next-generation ORM for type-safe database querying and management.
+### Database
+- PostgreSQL: Robust, high-performance, open-source relational database.
+### Authentication
+- JWT (JSON Web Tokens): Secure method for transmitting information between client and server.
+
+This stack provides a modern, scalable, and maintainable solution for building a full-stack blog website. Each component has been chosen to ensure high performance, security, and developer productivity.
+
+
+## Getting Started
+- Setting up backend first with hono@latest.
+- Setting up required routes.
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,70 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { jwt, sign } from 'hono/jwt'
 
-const app = new Hono()
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  }
+}>();
 
-app.post('/api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post('/api/v1/user/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+
+    const token = await sign({id: user.id }, c.env.JWT_SECRET);
+    return c.json({
+      jwt: token
+    });
+  } catch (error) {
+    c.status(411);
+    return c.text("Invalid");
+  }
+});
+
+app.post('/api/v1/user/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+        password: body.password
+      }
+    });
+  
+    if(!user) {
+      c.status(403);
+      return c.json({ message: "Incorrect Credentials" });
+    }
+  
+    const token = await sign({id: user.id}, c.env.JWT_SECRET);
+    return c.json({
+      jwt: token
+    });
+  } catch (error) {
+    c.status(411);
+    return c.text("Invalid");
+  }  
+});
 
 app.post('/api/v1/blog', (c) => {
   return c.text('Hello Hono!')
