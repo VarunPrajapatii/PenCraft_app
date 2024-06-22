531eb00b24edc7afde1fd1d201d929144119cc0a
Varun Prajapati <varunprajapati123@gmail.com>
2024-06-22 12:54:51 +0530

    added common zod and npm published

---
+++ common/src/index.ts
@@ -0,0 +1,33 @@
+import z from "zod";
+
+export const signupInput = z.object({
+    username: z.string().email(),
+    password: z.string().min(6),
+    name: z.string().optional()
+});
+
+
+export const signinInput = z.object({
+    username: z.string().email(),
+    password: z.string().min(6),
+});
+
+
+export const createPostInput = z.object({
+    title: z.string(),
+    content: z.string(),
+});
+
+
+export const updatePostInput = z.object({
+    title: z.string(),
+    content: z.string(),
+    id: z.string(),
+});
+
+
+
+export type SignupInput = z.infer<typeof signupInput>;
+export type SigninInput = z.infer<typeof signinInput>;
+export type CreatePostInput = z.infer<typeof createPostInput>;
+export type UpdatePostInput = z.infer<typeof updatePostInput>;
