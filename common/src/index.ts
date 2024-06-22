98e8cad816218b038c2c5dd198e07866dda2dbb5
Varun Prajapati <varunprajapati123@gmail.com>
2024-06-22 17:25:50 +0530

    username changed to email

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,14 +1,14 @@
 import z from "zod";
 
 export const signupInput = z.object({
-    username: z.string().email(),
+    email: z.string().email(),
     password: z.string().min(6),
     name: z.string().optional()
 });
 
 
 export const signinInput = z.object({
-    username: z.string().email(),
+    email: z.string().email(),
     password: z.string().min(6),
 });
 
