51ff568610e3863a58b91baae38cb85c32c0cbc8
Varun Prajapati <varunprajapati123@gmail.com>
2024-07-22 14:46:32 +0530

    changes to deploy it for the first time

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -9,7 +9,7 @@ export const AppBar = () => {
             </Link>
             <div>
                 <Link to={"/publish"} >
-                    <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm mr-16 px-5 py-2.5 me-2 mb-2">Green</button>
+                    <button type="button" className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm mr-16 px-5 py-2.5 me-2 mb-2">Create blog</button>
                 </Link>
                 <Avatar name={"Varun Prajapati"} size={"big"} />
             </div>
