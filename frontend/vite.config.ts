70f7943f3226a3179fbdf8998c5278055d2cc942
Varun Prajapati <varunprajapati123@gmail.com>
2025-06-23 14:12:34 +0530

    refactor (tailwind): migrated to tailwind v4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwind from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwind()],
+  server: {
+    host: '0.0.0.0',
+    // This is while Development, to access frontend from other devices for responsive testing
+  }
 })
