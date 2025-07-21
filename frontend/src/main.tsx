aedfdd3b153ed51078c7477440f17f350f0bd7b4
Varun Prajapati <varunprajapati123@gmail.com>
2025-07-21 13:01:07 +0530

    ops: final fe cd test

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './app.css'
+import './App.css'
 import { Provider } from 'react-redux'
 import store from './redux/store.ts'
 
